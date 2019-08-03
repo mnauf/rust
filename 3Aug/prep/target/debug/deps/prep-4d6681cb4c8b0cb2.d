71d9286f46910e69f1e60315affa67064f67d42d
Muhammad Naufil <m.naufil1@gmail.com>
2019-08-03 15:49:12 +0500

    More

---
+++ 3Aug/prep/target/debug/deps/prep-4d6681cb4c8b0cb2.d
@@ -0,0 +1,5 @@
+/home/naufil/Desktop/rust_github/3Aug/prep/target/debug/deps/prep-4d6681cb4c8b0cb2: src/main.rs
+
+/home/naufil/Desktop/rust_github/3Aug/prep/target/debug/deps/prep-4d6681cb4c8b0cb2.d: src/main.rs
+
+src/main.rs:
---
+++ 3Aug/prep/target/debug/prep.d
@@ -0,0 +1,1 @@
+/home/naufil/Desktop/rust_github/3Aug/prep/target/debug/prep: /home/naufil/Desktop/rust_github/3Aug/prep/src/main.rs
