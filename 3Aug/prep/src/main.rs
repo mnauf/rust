cf51aad7d134c53c6adff628b244e26140ec1a40
Muhammad Naufil <m.naufil1@gmail.com>
2019-08-03 15:56:33 +0500

    Largest number program

--- 3Aug/prep/src/main.rs
+++ 3Aug/prep/src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let h = vec![1,2,3,4];
+    let h = vec![4,3,2,1];
     println!("{:?}",h);
+    let mut largest = h[0];
+    for number in h
+    {
+        if number > largest{
+            largest = number;
+        };
+    };
+    println!("The largest number in list is: {}",largest);
 }
