329ea971af330ab370585b404759483ec442f381
Muhammad Naufil <m.naufil1@gmail.com>
2019-08-05 20:12:46 +0500

    multiple type parameters in struct

---
+++ 5Aug/multiple_generics_in_rust/target/debug/deps/multiple_generics_in_rust-bd9ca7b36bd5cd2d.d
@@ -0,0 +1,5 @@
+/home/naufil/Desktop/rust_github/5Aug/multiple_generics_in_rust/target/debug/deps/multiple_generics_in_rust-bd9ca7b36bd5cd2d: src/main.rs
+
+/home/naufil/Desktop/rust_github/5Aug/multiple_generics_in_rust/target/debug/deps/multiple_generics_in_rust-bd9ca7b36bd5cd2d.d: src/main.rs
+
+src/main.rs:
---
+++ 5Aug/multiple_generics_in_rust/target/debug/multiple_generics_in_rust.d
@@ -0,0 +1,1 @@
+/home/naufil/Desktop/rust_github/5Aug/multiple_generics_in_rust/target/debug/multiple_generics_in_rust: /home/naufil/Desktop/rust_github/5Aug/multiple_generics_in_rust/src/main.rs
