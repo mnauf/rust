8adba9a37dca3ea4789848837b57b1e2c71307be
Muhammad Naufil <m.naufil1@gmail.com>
2019-08-06 17:07:12 +0500

    going for quiz 5

--- 6Aug/threads/src/main.rs
+++ 6Aug/threads/src/main.rs
@@ -1,16 +1,25 @@
-use std::thread;
-use std::time::Duration;
+// use std::thread;
+// use std::time::Duration;
 
-fn main() {
-    thread::spawn(|| {
-        for i in 1..50 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..40 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
+// fn main() {
+//     let handle = thread::spawn(||{
+//         for i in 0..9 {
+//             println!("From spawn thread counting: {}",i);
+//             thread::sleep(Duration::from_millis(1));
+//         };
+//     });
+//     for i in 0..5 {
+//         println!("From main thread counting: {}",i);
+//         thread::sleep(Duration::from_millis(1));
+//     }
+//     handle.join().unwrap();
+// }
+fn trying(x: u32)->u32{
+    x
+}
+fn main()
+{
+    let x = 32;
+    let y = trying(x);
+    println!("{}",y);
 }
