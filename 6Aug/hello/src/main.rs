8e4f7ddfd31ef8eb839f308bb2ad0ffafcc7ad11
Muhammad Naufil <m.naufil1@gmail.com>
2019-08-06 12:18:26 +0500

    finished lifetimes

--- 6Aug/hello/src/main.rs
+++ 6Aug/hello/src/main.rs
@@ -1,18 +1,37 @@
-fn main() {
-    let string1 = String::from("long string is long");
-    let result;
-    let string2 = String::new();
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
+// fn main() {
+//     let string1 = String::from("long string is long");
+//     let result;
+//     {
+//         let string2 = String::from("xyz");
+//         result = longest(string1.as_str(), string2.as_str());
+//     }
+//     println!("The longest string is {}", result);
+// }
+
+// fn longest<'a>(x: &'a str, y: &str) -> String {
+//     format!("the two strings are {} and {}",x.as_string(),y.as_string())
+// }
+
+// // fn main() {
+// //     let string1 = String::from("I love Pakistan");
+// //     let result;
+// //     {
+// //     let string2 = String::from("Hello world");
+// //     let result = longest(string1.as_str(),string2.as_str());
+    
+// //     }
+// //     println!("The longest is: {}",result);
+// // }
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str
 }
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+    println!("{:?}",i);
 }
---
+++ 6Aug/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/naufil/Desktop/rust_github/6Aug/hello/target/debug/hello: /home/naufil/Desktop/rust_github/6Aug/hello/src/main.rs
