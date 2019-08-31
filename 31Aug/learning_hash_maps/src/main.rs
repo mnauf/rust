96168f43a46f34917dcda85d88e5f877e399ab3d
Muhammad Naufil <m.naufil1@gmail.com>
2019-08-31 16:38:01 +0500

    Hashmaps word

--- 31Aug/learning_hash_maps/src/main.rs
+++ 31Aug/learning_hash_maps/src/main.rs
@@ -43,20 +43,83 @@
 // scores.insert(String::from("Yellow"), 50);
 
 // let team_name = String::from("Blue");
+// let score = scores.get(team_name); // This method only accepts the reference
+// println!("{:?}",score);
+// }
+
+// Program 5
+// fn main() {
+// use std::collections::HashMap;
+
+// let mut scores = HashMap::new();
+
+// scores.insert(String::from("Blue"), 10);
+// scores.insert(String::from("Yellow"), 50);
+
+// let team_name = String::from("Blue");
 // let score = scores.get(&team_name);
 // println!("{:?}",score);
 // }
 
-#![allow(unused_variables)]
-fn main() {
-use std::collections::HashMap;
+//Program 6
+// fn main() {
+//     use std::collections::HashMap;
 
-let mut scores = HashMap::new();
+//     let mut scores = HashMap::new();
 
-scores.insert(String::from("Blue"), 10);
-scores.insert(String::from("Yellow"), 50);
+//     scores.insert(String::from("Blue"), 10);
+//     scores.insert(String::from("Yellow"), 50);
 
-for (key, value) in &scores {
-    println!("{}: {}", key, value);
-}
-}
+//     for (key, value) in &scores {
+//         println!("{}: {}", key, value);
+//     }
+// }
+
+//Program 7
+// fn main() {
+//     use std::collections::HashMap;
+
+// let mut scores = HashMap::new();
+
+//     scores.insert(String::from("Blue"), 10);
+//     scores.insert(String::from("Yellow"), 50);
+
+//     for (key, value) in &scores {
+//         println!("{}: {}", key, value);
+//     }
+// }
+
+// Program 8
+
+// use std::collections::HashMap;
+// fn main() {
+// let mut scores = HashMap::new();
+// scores.insert(String::from("Blue"), 10);
+// scores.insert(String::from("Blue"), 25);
+
+// println!("{:?}", scores);
+// }
+
+// Program 9
+// fn main() {
+// use std::collections::HashMap;
+// let mut scores = HashMap::new();
+// scores.insert(String::from("Blue"), 10);
+// scores.entry(String::from("Yellow")).or_insert(50);
+// scores.entry(String::from("Blue")).or_insert(50);
+// println!("{:?}", scores);
+// }
+// Program 10
+// use std::collections::HashMap;
+// fn main() {
+//     let text = "hello world wonderful world";
+
+//     let mut map = HashMap::new();
+
+//     for word in text.split_whitespace() {
+//         let count = map.entry(word).or_insert(0);
+//         *count += 1;
+//     }
+
+//     println!("{:?}", map);
+// }
