ec96a43bd9ad8fb997dedc487261a83f6abb0786
Muhammad Naufil <m.naufil1@gmail.com>
2019-11-23 12:25:44 +0500

    23Nov1

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,30 @@
-#[derive(Debug)]
-struct Point<T,U>{
-    x: T,
-    y:U
+// #[derive(Debug)]
+// struct Point<T,U>{
+//     x: T,
+//     y:U
+// }
+// fn main() {
+//     let point_1 = Point{
+//         x:5.0,
+//         y:3
+//     };
+//     println!("{:?}",point_1)
+
+// }
+struct Point {
+    x: u32,
+    y: u32
+}
+impl Point {
+    fn x(&self) ->u32{
+        self.x
+    }
 }
 fn main() {
-    let point_1 = Point{
-        x:5.0,
-        y:3
+    let point_1 = Point {
+        x:32,
+        y:56
     };
-    println!("{:?}",point_1)
+    println!("{}",point_1.x())
 
 }
